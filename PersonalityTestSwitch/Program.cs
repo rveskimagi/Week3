fa4079a933c6b1e2649a2acf6e82f833b4c70a86
rvesk <jksdgn@gmail.com>
2022-02-16 07:56:27 -0500

    add new projects

---
+++ PersonalityTestSwitch/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace PersonalityTestSwitch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta oma lemmikvärv.");
+            string userColor = Console.ReadLine().ToLower();
+
+            switch (userColor)
+            {
+                case "punane":
+                    Console.WriteLine("Oled romantiline.");
+                    break;
+                case "sinine":
+                    Console.WriteLine("Oled töökas.");
+                    break;
+                case "roheline":
+                    Console.WriteLine("Oled looduse sõber.");
+                    break;
+                default:
+                    Console.WriteLine($"Oled {userColor} ükssarvik");
+                    break;
+
+            }
+
+            Console.WriteLine("Kena päeva!");
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
